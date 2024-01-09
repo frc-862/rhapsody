65eecc5270b07b69e6bda2b42264e715106f5a4c
MattD8957 <Dowlmj8957@gmail.com>
2024-01-09 17:44:14 -0500

    [#5][#9] Added CTRE Swerve

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,13 +1,46 @@
 package frc.robot;
 
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.ControllerConstants;
+import frc.robot.Constants.TunerConstants;
+import frc.robot.Constants.drivetrainConstants;
+import frc.robot.subsystems.Swerve;
 import frc.thunder.LightningContainer;
 
 public class RobotContainer extends LightningContainer {
-    @Override
-    protected void configureButtonBindings() {}
 
+    /* Setting up bindings for necessary control of the swerve drive platform */
+    XboxController driver = new XboxController(ControllerConstants.DriverControllerPort); // My joystick
+    Swerve drivetrain = TunerConstants.DriveTrain; // My drivetrain
+
+	SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric(); //TODO I want field-centric driving in open loop   WE NEED TO FIGURE OUT WHAT Change beacuse with open loop is gone
+  	SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
+  	SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+  	Telemetry logger = new Telemetry(drivetrainConstants.MaxSpeed);
+
+	
+	@Override
+	protected void configureButtonBindings() {	  
+		new Trigger(driver::getAButton).whileTrue(drivetrain.applyRequest(() -> brake));
+		new Trigger(driver::getBButton).whileTrue(drivetrain.applyRequest(() -> point.withModuleDirection(new Rotation2d(-driver.getLeftY(), -driver.getLeftX()))));
+		new Trigger(driver::getXButton).onTrue(new InstantCommand(() -> drivetrain.zeroGyro())); // TODO create function to reset Heading
+		drivetrain.registerTelemetry(logger::telemeterize);
+    }
+	
     @Override
-    protected void configureDefaultCommands() {}
+    protected void configureDefaultCommands() {
+		drivetrain.setDefaultCommand( // Drivetrain will execute this command periodically
+		  drivetrain.applyRequest(() -> drive.withVelocityX(
+			-MathUtil.applyDeadband(driver.getLeftY(), 0.1) * drivetrainConstants.MaxSpeed) // Drive forward with negative Y (forward)
+			  .withVelocityY(-MathUtil.applyDeadband(driver.getLeftX(), 0.1) * drivetrainConstants.MaxSpeed) // Drive left with negative X (left)
+			  .withRotationalRate(-MathUtil.applyDeadband(driver.getRightX(), 0.1) * drivetrainConstants.MaxAngularRate) // Drive counterclockwise with negative X (left)
+		  ));
+	}
 
     @Override
     protected void configureAutonomousCommands() {}
