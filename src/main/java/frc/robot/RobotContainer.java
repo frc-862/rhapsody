70f8e212428365baaa672bf2cb867b10d136a166
Nate Bailey <nabaile586@gmail.com>
2024-01-10 09:30:10 -0500

    [#11] added supply and satury limits into shooter motor creation

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -2,6 +2,8 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.ControllerConstants;
+
 import frc.robot.command.Shoot;
 import frc.robot.subsystem.Shooter;
 import frc.thunder.LightningContainer;
--- src/main/java/frc/robot/subsystem/Shooter.java
+++ src/main/java/frc/robot/subsystem/Shooter.java
@@ -7,10 +7,9 @@ package frc.robot.subsystem;
 import com.ctre.phoenix6.controls.VelocityVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
 
-import edu.wpi.first.units.Velocity;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants.ShooterConstants;
 import frc.robot.Constants.RobotMap.CAN;
+import frc.robot.Constants.ShooterConstants;
 import frc.thunder.config.FalconConfig;
 
 public class Shooter extends SubsystemBase {
@@ -20,8 +19,8 @@ public class Shooter extends SubsystemBase {
   final VelocityVoltage rpmTarget = new VelocityVoltage(0).withSlot(0);
   
   public Shooter() {
-    shooterMotor1 = FalconConfig.createMotor(CAN.SHOOTER_MOTOR_1, getName(), ShooterConstants.MOTOR_1_INVERT, ShooterConstants.NEUTRAL_MODE, ShooterConstants.KP, ShooterConstants.KI, ShooterConstants.KD, ShooterConstants.KS, ShooterConstants.KV);
-    shooterMotor2 = FalconConfig.createMotor(CAN.SHOOTER_MOTOR_2, getName(), ShooterConstants.MOTOR_2_INVERT, ShooterConstants.NEUTRAL_MODE, ShooterConstants.KP, ShooterConstants.KI, ShooterConstants.KD, ShooterConstants.KS, ShooterConstants.KV);
+    shooterMotor1 = FalconConfig.createMotor(CAN.SHOOTER_MOTOR_1, getName(), ShooterConstants.MOTOR_1_INVERT, ShooterConstants.SUPPLY_CURRENT_LIMIT, ShooterConstants.STATOR_CURRENT_LIMIT, ShooterConstants.NEUTRAL_MODE, ShooterConstants.KP, ShooterConstants.KI, ShooterConstants.KD, ShooterConstants.KS, ShooterConstants.KV);
+    shooterMotor2 = FalconConfig.createMotor(CAN.SHOOTER_MOTOR_1, getName(), ShooterConstants.MOTOR_2_INVERT, ShooterConstants.SUPPLY_CURRENT_LIMIT, ShooterConstants.STATOR_CURRENT_LIMIT, ShooterConstants.NEUTRAL_MODE, ShooterConstants.KP, ShooterConstants.KI, ShooterConstants.KD, ShooterConstants.KS, ShooterConstants.KV);
   }
   public void setRPM(double rpm){
     shooterMotor1.setControl(rpmTarget.withVelocity(rpm).withFeedForward(0.5));
