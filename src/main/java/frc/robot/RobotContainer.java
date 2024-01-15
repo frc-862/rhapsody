5c59ccf309fd20715a2fafa348cd47e6a74947a9
MattD8957 <Dowlmj8957@gmail.com>
2024-01-14 22:01:55 -0500

    [#22] Added Basic Paths to test, and a path chooser

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -1,11 +1,17 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.Flywheel;
 import frc.robot.subsystems.Pivot;
 
 import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.auto.NamedCommands;
+
 import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.XboxController;
@@ -14,6 +20,8 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.command.*;
 import frc.robot.subsystems.Collector;
 import frc.thunder.LightningContainer;
+import frc.thunder.shuffleboard.LightningShuffleboard;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Constants.ControllerConstants;
@@ -21,8 +29,11 @@ import frc.robot.Constants.TunerConstants;
 import frc.robot.Constants.drivetrainConstants;
 import frc.robot.subsystems.Swerve;
 import frc.thunder.LightningContainer;
+import frc.thunder.shuffleboard.LightningShuffleboard;
 
 public class RobotContainer extends LightningContainer {
+	private final SendableChooser<Command> autoChooser = AutoBuilder.buildAutoChooser();
+
     /* Setting up bindings for necessary control of the swerve drive platform */
     XboxController driver = new XboxController(ControllerConstants.DriverControllerPort); // Driver controller
     XboxController coPilot = new XboxController(ControllerConstants.CopilotControllerPort); // CoPilot controller
@@ -36,8 +47,15 @@ public class RobotContainer extends LightningContainer {
     Collector collector = new Collector();
 
 	
+	
+
 	@Override
 	protected void configureButtonBindings() {
+		NamedCommands.registerCommand("CollectIN", new Collect(collector, () -> 1d));
+
+		LightningShuffleboard.set("Auton", "Auto Chooser", autoChooser);
+
+
 		new Trigger(driver::getAButton).whileTrue(drivetrain.applyRequest(() -> brake));
 		new Trigger(driver::getBButton).whileTrue(drivetrain.applyRequest(() -> point.withModuleDirection(new Rotation2d(-driver.getLeftY(), -driver.getLeftX()))));
 		new Trigger(driver::getXButton).onTrue(new InstantCommand(() -> drivetrain.zeroGyro())); // TODO create function to reset Heading
@@ -60,7 +78,9 @@ public class RobotContainer extends LightningContainer {
 	}
 
         @Override
-        protected void configureAutonomousCommands() {}
+        protected Command configureAutonomousCommands() {
+			return autoChooser.getSelected();
+		}
 
         @Override
         protected void releaseDefaultCommands() {}
