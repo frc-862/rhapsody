4f3a2012aac74089f183a78c4650c9113b91917c
Evan Leroy <evanleroy208@gmail.com>
2024-01-22 20:16:28 -0500

    [66] Comment useless Collision java file

--- src/main/java/frc/robot/command/TipDetection.java
+++ src/main/java/frc/robot/command/TipDetection.java
@@ -35,9 +35,7 @@ public class TipDetection extends Command {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
--- src/main/java/frc/robot/subsystems/Collision.java
+++ src/main/java/frc/robot/subsystems/Collision.java
@@ -1,7 +1,7 @@
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
-
+/*
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
@@ -32,3 +32,4 @@ public class Collision extends SubsystemBase {
     roll = drivetrain.getPigeon2().getRoll().getValueAsDouble();
   }
 }
+*/
