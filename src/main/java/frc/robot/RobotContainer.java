b95605f8247ee74c99b4b7e7b2d7459d7f1ff35f
vilok1 <vgunti777@gmail.com>
2024-01-24 20:15:50 -0500

    [#86] added climb subsystem

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import frc.robot.Constants.DrivetrAinConstants;
 import frc.robot.Constants.TunerConstants;
 import frc.robot.command.PointAtTag;
 import frc.robot.command.Shoot;
+import frc.robot.subsystems.Climb;
 import frc.robot.subsystems.Collector;
 import frc.robot.subsystems.Collision;
 import frc.robot.subsystems.Flywheel;
@@ -37,6 +38,7 @@ public class RobotContainer extends LightningContainer {
 	// Shooter shooter;
 	// Collision collision;
 	// Indexer indexer;
+	Climb climb;
 
 	private SendableChooser<Command> autoChooser;
 	// TODO I want field-centric driving in open loop WE NEED TO FIGURE OUT WHAT
@@ -64,6 +66,7 @@ public class RobotContainer extends LightningContainer {
 		// pivot = new Pivot();
 		// shooter = new Shooter(pivot, flywheel, indexer);
 		// collision = new Collision(drivetrain);
+		climb = new Climb();
 
 		drive = new SwerveRequest.FieldCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);//.withDeadband(DrivetrAinConstants.MaxSpeed * DrivetrAinConstants.SPEED_DB).withRotationalDeadband(DrivetrAinConstants.MaxAngularRate * DrivetrAinConstants.ROT_DB); // I want field-centric driving in closed loop
 		slow = new SwerveRequest.FieldCentric().withDriveRequestType(DriveRequestType.OpenLoopVoltage);//.withDeadband(DrivetrAinConstants.MaxSpeed * DrivetrAinConstants.SPEED_DB).withRotationalDeadband(DrivetrAinConstants.MaxAngularRate * DrivetrAinConstants.ROT_DB); // I want field-centric driving in closed loop
---
+++ src/main/java/frc/robot/subsystems/Climb.java
@@ -0,0 +1,54 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import com.ctre.phoenix6.hardware.TalonFX;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.ClimbConstants;
+import frc.robot.Constants.RobotMap.CAN;
+import frc.thunder.config.FalconConfig;
+
+public class Climb extends SubsystemBase {
+  /** Creates a new Climb. */
+  public TalonFX climbMotorR;
+  public TalonFX climbMotorL;
+
+  public Climb() {
+    // configure climb motors
+    climbMotorR = FalconConfig.createMotor(CAN.CLIMB_RIGHT, CAN.CANBUS_FD,
+      ClimbConstants.CLIMB_RIGHT_MOTOR_INVERT, ClimbConstants.CLIMB_MOTOR_SUPPLY_CURRENT_LIMIT, 
+      ClimbConstants.CLIMB_MOTOR_STATOR_CURRENT_LIMIT, ClimbConstants.FLYWHEEL_MOTOR_NEUTRAL_MODE, ClimbConstants.CLIMB_MOTOR_KP,
+      ClimbConstants.CLIMB_MOTOR_KI, ClimbConstants.CLIMB_MOTOR_KD, ClimbConstants.CLIMB_MOTOR_KS, 
+      ClimbConstants.CLIMB_MOTOR_KV);
+    climbMotorL = FalconConfig.createMotor(CAN.CLIMB_LEFT, CAN.CANBUS_FD,
+      ClimbConstants.CLIMB_LEFT_MOTOR_INVERT, ClimbConstants.CLIMB_MOTOR_SUPPLY_CURRENT_LIMIT, 
+      ClimbConstants.CLIMB_MOTOR_STATOR_CURRENT_LIMIT, ClimbConstants.FLYWHEEL_MOTOR_NEUTRAL_MODE, ClimbConstants.CLIMB_MOTOR_KP,
+      ClimbConstants.CLIMB_MOTOR_KI, ClimbConstants.CLIMB_MOTOR_KD, ClimbConstants.CLIMB_MOTOR_KS, 
+      ClimbConstants.CLIMB_MOTOR_KV);
+  }
+
+  public void setClimbPower(double power) {
+      climbMotorR.set(power);
+      climbMotorL.set(power);
+  }
+
+  public void extendClimb(){
+    setClimbPower(ClimbConstants.CLIMB_DEFAULT_EXTEND_POWER);
+  }
+
+  public void retractClimb() {
+    setClimbPower(ClimbConstants.CLIMB_DEFAULT_RETRACT_POWER);
+  }
+
+  public void stopClimb(){
+    setClimbPower(0d);
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
