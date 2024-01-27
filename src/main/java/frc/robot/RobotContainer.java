3195e73d6944b3ae41f9d79dc86692fd11ae2370
WindowsVistaisCool <kyle@quackings.com>
2024-01-27 14:49:51 -0500

    [#118] fix constants ref

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -169,12 +169,12 @@ public class RobotContainer extends LightningContainer {
 
 	@Override
 	protected void configureSystemTests() {
-		SystemTest.registerTest("Drive Test", new DrivetrainSystemTest(drivetrain, brake, Constants.DrivetrainConstants.SYS_TEST_SPEED_DRIVE));
+		SystemTest.registerTest("Drive Test", new DrivetrainSystemTest(drivetrain, brake, DrivetrainConstants.SYS_TEST_SPEED_DRIVE));
 
 		SystemTest.registerTest("Azimuth Test", new SequentialCommandGroup(
-			new TimedCommand(new TurnSystemTest(drivetrain, () -> Constants.DrivetrainConstants.SYS_TEST_SPEED_TURN), 1),
+			new TimedCommand(new TurnSystemTest(drivetrain, () -> DrivetrainConstants.SYS_TEST_SPEED_TURN), 1),
 			new WaitCommand(0.5),
-			new TimedCommand(new TurnSystemTest(drivetrain, () -> -Constants.DrivetrainConstants.SYS_TEST_SPEED_TURN), 1),
+			new TimedCommand(new TurnSystemTest(drivetrain, () -> -DrivetrainConstants.SYS_TEST_SPEED_TURN), 1),
 			drivetrain.applyRequest(() -> brake)
 		));
 	}
