b11021170379407eaaf44da3be5937c6f550d6fe
Fritz Geib <fritz@standland.org>
2024-01-28 23:35:05 -0500

    [#127] cleanse climb commands

--- src/main/java/frc/robot/command/ManualClimb.java
+++ src/main/java/frc/robot/command/ManualClimb.java
@@ -1,57 +1,41 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.command;
 
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.subsystems.Climber;
-import frc.thunder.shuffleboard.LightningShuffleboard;
 
 public class ManualClimb extends Command {
 
-  private Climber climber;
-  private DoubleSupplier powerSupplier;
+    private Climber climber;
+    private DoubleSupplier leftPower;
+    private DoubleSupplier rightPower;
 
-  /**
-   * Creates a new ManualClimb.
-   * @param powerSupplier DoubleSupplier for power of motor (-1 to 1)
-   * @param climber subsystem
-   */
-  public ManualClimb(DoubleSupplier powerSupplier, Climber climber) {
-    // Use addRequirements() here to declare subsystem dependencies.
-    this.powerSupplier = powerSupplier;
-    this.climber = climber;
+    /**
+     * Creates a new ManualClimb.
+     * @param powerSupplier DoubleSupplier for power of motor (-1 to 1)
+     * @param climber subsystem
+     */
+    public ManualClimb(DoubleSupplier leftPower, DoubleSupplier rightPower, Climber climber) {
+        // Use addRequirements() here to declare subsystem dependencies.
+        this.climber = climber;
+        this.leftPower = leftPower;
+        this.rightPower = rightPower;
+        addRequirements(climber);
+    }
 
-    addRequirements(climber);
-  }
+    @Override
+    public void execute() {
+        climber.setPower(leftPower.getAsDouble(), rightPower.getAsDouble());
+    }
 
-  // Called when the command is initially scheduled.
-  @Override
-  public void initialize() {
-    climber.setPower(powerSupplier.getAsDouble());
+    @Override
+    public void end(boolean interrupted) {
+        climber.stop();
+    }
 
-    LightningShuffleboard.setDoubleSupplier("ManualClimb", "Climb Height", () -> climber.getHeight());
-    LightningShuffleboard.setDoubleSupplier("ManualClimb", "Climb Power", () -> powerSupplier.getAsDouble());
-  }
-
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-    climber.setPower(powerSupplier.getAsDouble());
-  }
-
-  // Called once the command ends or is interrupted.
-  @Override
-  public void end(boolean interrupted) {
-    climber.stopClimb();
-  }
-
-  // Returns true when the command should end.
-  @Override
-  public boolean isFinished() {
-    return false;
-  }
+    @Override
+    public boolean isFinished() {
+        return false;
+    }
 }
