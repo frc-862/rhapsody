72082eddf6b99de8f6eef9ddc96829714c8dff40
Meh243 <120067923+Meh243@users.noreply.github.com>
2024-02-01 19:03:33 -0500

    [#146] deleted disable and enable vision triggers

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -102,10 +102,6 @@ public class RobotContainer extends LightningContainer {
 	
 		new Trigger(driver::getXButton).whileTrue(new ChasePieces(drivetrain));
 		new Trigger(driver::getBackButton).whileTrue(new TipDetection(drivetrain));
-
-		new Trigger(driver::getXButton).onTrue(new InstantCommand(() -> drivetrain.disableVision()));
-		new Trigger(driver::getYButton).onTrue(new InstantCommand(() -> drivetrain.enableVision()));
-		// new Trigger(driver::getYButton).whileTrue(new Climb(climber, drivetrain));
 	}
 
 	@Override
