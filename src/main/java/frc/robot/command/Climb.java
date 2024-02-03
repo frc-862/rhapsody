674274b5a9a459d361305970aaaa1d3b08494ee7
Driver Station <126530662+driverstation862@users.noreply.github.com>
2024-02-03 13:56:03 -0500

    [#130] added an abort feature and made climber code onTrue

--- src/main/java/frc/robot/command/Climb.java
+++ src/main/java/frc/robot/command/Climb.java
@@ -5,11 +5,11 @@ import frc.robot.Constants.ClimbConstants;
 import frc.robot.Constants.ClimbConstants.CLIMBER_STATES;
 import frc.robot.subsystems.Climber;
 import frc.robot.subsystems.Swerve;
-
 public class Climb extends Command {
 	private Climber climber;
 	boolean hasTipped = false;
 	private TipDetection tipDetection;
+	BooleanSupplier abort;
 
 	/**
 	 * Creates a new Climb.
@@ -17,10 +17,10 @@ public class Climb extends Command {
 	 * @param climber subsystem
 	 * @param drivetrain subsystem
 	 */
-	public Climb(Climber climber, Swerve drivetrain) {
+	public Climb(Climber climber, Swerve drivetrain, BooleanSupplier abort) {
 		this.climber = climber;
 		this.tipDetection = new TipDetection(drivetrain);
-
+		this.abort = abort;
 		addRequirements(climber);
 	}
 
@@ -28,8 +28,12 @@ public class Climb extends Command {
 	public void initialize() {
 		// extend arm is preperation for climbing
 		if (climber.getState() == CLIMBER_STATES.STOW){
-		climber.setSetpoint(ClimbConstants.MAX_HEIGHT);
+			climber.setSetpoint(ClimbConstants.MAX_HEIGHT);
+			climber.hasPressed();
 		}
+
+
+
 	}
 
 	@Override
@@ -43,10 +47,11 @@ public class Climb extends Command {
 					tipDetection.isTipped()) {
 					climber.setSetpoint(0d);
 					climber.setHasTipped(true);
+					
 				}
 				break;
 			case CLIMBED:
-				// re-extend arm slowly to retun to ground
+				// re-extend arm slowly to return to ground
 				if (ClimbConstants.MAX_HEIGHT - climber.getHeightR() >= ClimbConstants.CLIMB_EXTENSION_TOLERANCE){
 					climber.setPowerR(ClimbConstants.CLIMB_RETURN_TO_GROUND_MAX_POWER);					
 				} 
@@ -71,12 +76,17 @@ public class Climb extends Command {
 	}
 
 	@Override
-	public void end(boolean interrupted) {
-		climber.stop();
+	public void end(boolean interrupted){
+		abort();
 	}
 
 	@Override
 	public boolean isFinished() {
-		return false;
+		return abort.getAsBoolean;
+	}
+
+	public void abort(){
+		Climber.setState(CLIMBER_STATES.CLIMBED);
+		climber.stop();
 	}
 }
