d2091f226fbf0055e0c4f9fabbdc08e2fd25fccf
MattD8957 <Dowlmj8957@gmail.com>
2024-02-06 10:23:33 -0500

    [#180] Added comments

--- src/main/java/frc/robot/subsystems/Limelights.java
+++ src/main/java/frc/robot/subsystems/Limelights.java
@@ -9,11 +9,11 @@ public class Limelights extends SubsystemBase {
     private Limelight champions;
 
     public Limelights() {
-        pressure = new Limelight("limelight-pressure", "10.8.62.11");
-        dust = new Limelight("limelight-dust", "10.8.62.12");
-        champions = new Limelight("limelight-champions", "10.8.62.13");
+        pressure = new Limelight("limelight-pressure", "10.8.62.11");   // LL3   Back
+        dust = new Limelight("limelight-dust", "10.8.62.12");           // LL2+  Front up
+        champions = new Limelight("limelight-champions", "10.8.62.13"); // LL2+  Front down (collector)
 
-        //TODO: make actual pipelines... maybe an enum?
+        //TODO: make actual pipelines... maybe an enum? At least use constants
         pressure.setPipeline(0);
         dust.setPipeline(0);
         champions.setPipeline(0);
