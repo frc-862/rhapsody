b0b1a29b433a4f5010f52261cd514f635e2d35b7
EvanLeroy <128870446+EvanLeroy@users.noreply.github.com>
2024-02-06 19:26:27 -0500

    [#33] commit

---
+++ src/main/java/frc/robot/command/Sing.java
@@ -0,0 +1,58 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.command;
+
+import com.ctre.phoenix6.Orchestra;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.subsystems.Swerve;
+
+public class Sing extends Command {
+
+  Orchestra sing = new Orchestra();
+  Swerve drivetrain;
+  String filepath;
+
+  /** Creates a new Sing. */
+  /*
+   * @param drivetrain used to grab the motors for the song
+   * @param filepath used to select what song to sing
+   * please note that this removes all usage of the drivetrain until it is ended.
+   * use with caution, and only when the drivetrain is not in use.
+   */
+  public Sing(Swerve drivetrain, String filepath) {
+    this.drivetrain = drivetrain;
+    this.filepath = filepath;
+  }
+
+  // Called when the command is initially scheduled.
+
+  @Override
+  public void initialize() {
+    for (int i = 0; i < 4; i++){
+      sing.addInstrument(drivetrain.getModule(i).getDriveMotor());
+      sing.addInstrument(drivetrain.getModule(i).getSteerMotor());
+    }
+    sing.loadMusic(filepath);
+    sing.play();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {}
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    sing.stop();
+    sing.clearInstruments();
+  }
+
+  // Returns true when gitthe command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
