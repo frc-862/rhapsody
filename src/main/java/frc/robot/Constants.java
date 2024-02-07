5f8dc4974500cdfdf2bacb080f3dc9234883ad5e
David <djreeves2005@gmail.com>
2024-02-06 19:18:47 -0500

    Co-authored-by: Carter <Carter30@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -307,6 +307,7 @@ public class Constants {
     public class MusicConstants {
         public static final String BOH_RHAP_FILEPATH = "bohemianrhapsody.chrp";
         public static final String JEOPARDY_FILEPATH = "jeopardy.chrp";
+        public static final String BEWARE_THE_FOREST_MUSHROOMS_FILEPATH = "bewaretheforestmushrooms.chrp";
     }
 
     public class CollectorConstants { // TODO: get real
