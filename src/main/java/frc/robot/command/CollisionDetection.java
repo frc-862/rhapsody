7d6bcc7ace1b4bcf801fa41bb4635b2639533f72
vilok1 <vgunti777@gmail.com>
2024-02-10 13:38:50 -0500

    [#151] added java docs

--- src/main/java/frc/robot/command/CollisionDetection.java
+++ src/main/java/frc/robot/command/CollisionDetection.java
@@ -10,6 +10,9 @@ import frc.robot.Constants.VisionConstants;
 import frc.robot.subsystems.CollisionDetector;
 import frc.robot.subsystems.Swerve;
 import frc.robot.Constants.TunerConstants;
+
+import com.ctre.phoenix6.mechanisms.swerve.SwerveRequest;
+
 import edu.wpi.first.math.util.Units;
 
 import frc.thunder.shuffleboard.LightningShuffleboard;
@@ -20,6 +23,7 @@ public class CollisionDetection extends Command {
   
 public Swerve drivetrain;
 public CollisionDetector collisionDetector;
+SwerveRequest.SwerveDriveBrake brake;
 
   public CollisionDetection(Swerve drivetrain, CollisionDetector collisionDetector) {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -31,6 +35,7 @@ public CollisionDetector collisionDetector;
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    brake = new SwerveRequest.SwerveDriveBrake();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -43,43 +48,70 @@ public CollisionDetector collisionDetector;
     LightningShuffleboard.setBool("Collision Detection", "collided", getIfCollided());
     
     if (getIfCollided()){
-      // do stuff
+      drivetrain.applyRequest(() -> brake);
     }
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
+  // COLLISION DETECTION
 
-  public double getPigeonAcceleration(){
+  /**
+   * @return acceleration from pigeon
+   */
+  public double getPigeonAcceleration(){  
 
     return Math.hypot(drivetrain.getPigeon2().getAccelerationX().getValueAsDouble(), 
     drivetrain.getPigeon2().getAccelerationY().getValueAsDouble()) * VisionConstants.ACCELERATION_DUE_TO_GRAVITY;
   }
 
+  /**
+   * gets acceleration from a drivemotor of the specified module
+   * @param moduleNumber
+   * @return motor acceleration
+   */
   public double getMotorAcceleration(int moduleNumber){
 
     return Units.rotationsToRadians(Math.abs(drivetrain.getModule(moduleNumber).getDriveMotor().getAcceleration().getValueAsDouble()) 
     * Units.inchesToMeters(TunerConstants.kWheelRadiusInches) / TunerConstants.kDriveGearRatio);
   }
 
+  /**
+   * compares acceleration of a specific drivemotor to pigeon and tolerance percentage in constants
+   * @param moduleNumber
+   * @return if motorAcceleration is within tolerance
+   */
   public boolean checkMotorAcceleration(int moduleNumber){
 
     return Math.abs(getPigeonAcceleration() - getMotorAcceleration(moduleNumber)) 
     > getMotorAcceleration(moduleNumber) * VisionConstants.Collision_Acceleration_Tolerance_Percentage;
   }
 
+  /**
+   * compares acceleration of a specific drivemotor to pigeon and given tolerance
+   * @param moduleNumber
+   * @param tolerance
+   * @return if motor is within tolerance
+   */
   public boolean checkMotorAcceleration(int moduleNumber, double tolerance){
 
     return Math.abs(getPigeonAcceleration() - getMotorAcceleration(moduleNumber)) > tolerance;
   }
 
+  /**
+   * @return if any motor has abnormal acceleration compared to pigeon and tolerance in constants
+   */
   public boolean getIfCollided(){
     
     return checkMotorAcceleration(0) || checkMotorAcceleration(1) 
     || checkMotorAcceleration(2) || checkMotorAcceleration(3);
   }
 
+  /**
+   * @param tolerance
+   * @return if any motor has abnormal acceleration compared to pigeon and given tolerance
+   */
   public boolean getIfCollided(double tolerance){
     
     return checkMotorAcceleration(0, tolerance) || checkMotorAcceleration(1, tolerance) 
