97745828a5bb8d4e3fcda0c1ece69a43601f6861
vilok1 <vgunti777@gmail.com>
2024-02-24 09:12:46 -0500

    [#151] filter motor values

--- src/main/java/frc/robot/command/CollisionDetection.java
+++ src/main/java/frc/robot/command/CollisionDetection.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.Constants.CollisionConstants;
 import frc.robot.subsystems.CollisionDetector;
 import frc.robot.subsystems.Swerve;
+import edu.wpi.first.math.filter.LinearFilter;
 import edu.wpi.first.math.util.Units;
 
 import frc.thunder.shuffleboard.LightningShuffleboard;
@@ -19,12 +20,19 @@ public class CollisionDetection extends Command {
   
 public Swerve drivetrain;
 public CollisionDetector collisionDetector;
-public double[] angularVelocityWorldLog = {0d, 0d};
-public double[] timeLog = {0d, 0d};
-public double[] robotRotationFromMotor = {0d, 0d};
-public double[] velocityXChassis = {0d, 0d};
-public double[] velocityYChassis = {0d, 0d};
-public double[] velocityRotChassis = {0d, 0d};
+public double[] rotVelP = {0d, 0d};
+public double[] time = {0d, 0d};
+public double[] xVelC = {0d, 0d};
+public double[] xVelY = {0d, 0d};
+public double[] rotVelC = {0d, 0d};
+
+public LinearFilter xAccCFilter;
+public LinearFilter yAccCFilter;
+public LinearFilter rotAccCFilter;
+
+public LinearFilter xAccC2Filter;
+public LinearFilter yAccC2Filter;
+public LinearFilter rotAccC2Filter;
 
   public CollisionDetection(Swerve drivetrain, CollisionDetector collisionDetector) {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -36,78 +44,107 @@ public double[] velocityRotChassis = {0d, 0d};
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    // store ang vel and time to calc angular acceleration
+    storeVelocities();
+    xAccCFilter = LinearFilter.singlePoleIIR(0.1, 0.01);
+    yAccCFilter = LinearFilter.singlePoleIIR(0.1, 0.01);
+    rotAccCFilter = LinearFilter.singlePoleIIR(0.1, 0.01);
 
-    angularVelocityWorldLog[1] = Units.degreesToRadians(drivetrain.getPigeon2().getAngularVelocityZDevice().getValueAsDouble());
-    timeLog[1] = Utils.getCurrentTimeSeconds();
-    robotRotationFromMotor[1] = drivetrain.getState().Pose.getRotation().getRadians();
-    velocityXChassis[1] = drivetrain.getCurrentRobotChassisSpeeds().vxMetersPerSecond;
-    velocityYChassis[1] = drivetrain.getCurrentRobotChassisSpeeds().vyMetersPerSecond;
-    velocityRotChassis[1] = drivetrain.getCurrentRobotChassisSpeeds().omegaRadiansPerSecond;
+    LinearFilter xAccC2Filter = LinearFilter.movingAverage(5);
+    LinearFilter yAccC2Filter = LinearFilter.movingAverage(5);
+    LinearFilter rotAccC2Filter = LinearFilter.movingAverage(5);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    storeVelocities();
 
-    // store ang vel and time to calc angular acceleration
-
-    angularVelocityWorldLog[0] = angularVelocityWorldLog[1];
-    timeLog[0] = timeLog[1];
-    robotRotationFromMotor[0] = robotRotationFromMotor[1];
-    velocityXChassis[0] = velocityXChassis[1];
-    velocityYChassis[0] = velocityYChassis[1];
-    velocityRotChassis[0] = velocityRotChassis[1];
-
-    angularVelocityWorldLog[1] = Units.degreesToRadians(drivetrain.getPigeon2().getAngularVelocityZDevice().getValueAsDouble());
-    timeLog[1] = Utils.getCurrentTimeSeconds();
-    robotRotationFromMotor[1] = drivetrain.getState().Pose.getRotation().getRadians();
-    velocityXChassis[1] = drivetrain.getCurrentRobotChassisSpeeds().vxMetersPerSecond;
-    velocityYChassis[1] = drivetrain.getCurrentRobotChassisSpeeds().vyMetersPerSecond;
-    velocityRotChassis[1] = drivetrain.getCurrentRobotChassisSpeeds().omegaRadiansPerSecond;
-
-    LightningShuffleboard.setDouble("Collision Detection", "total pidgeon acceleration", getPigeonAcceleration()[2]);
+    LightningShuffleboard.setDouble("Collision Detection", "pidgeon acceleration magnitude", getPigeonAcceleration()[2]);
     LightningShuffleboard.setDouble("Collision Detection", "pigeon accelaration direction", getPigeonAcceleration()[3]);
     LightningShuffleboard.setDouble("Collision Detection", "pigeon anglular acceleration", getPigeonAcceleration()[4]);
     LightningShuffleboard.setDouble("Collision Detection", "pigeon angular velocity", Units.degreesToRadians(drivetrain.getPigeon2().getAngularVelocityZDevice().getValueAsDouble()));
     LightningShuffleboard.setDouble("Collision Detection", "yaw", drivetrain.getPigeon2().getYaw().getValueAsDouble());
-    LightningShuffleboard.setDouble("Collision Detection", "motor acceleration magnitude", getChassisAcceleration()[2]);
-    LightningShuffleboard.setDouble("Collision Detection", "motor angular acceleration", getChassisAcceleration()[4]);
+    LightningShuffleboard.setDouble("Collision Detection", "motor acceleration magnitude(exp filter)", getChassisAcceleration()[2]);
+    LightningShuffleboard.setDouble("Collision Detection", "motor angular acceleration(exp filter)", getChassisAcceleration()[4]);
+    LightningShuffleboard.setDouble("Collision Detection", "motor acceleration magnitude(avg filter)", getChassisAcceleration2()[2]);
+    LightningShuffleboard.setDouble("Collision Detection", "motor angular acceleration(avg filter)", getChassisAcceleration2()[4]);
     LightningShuffleboard.setBool("Collision Detection", "collided", getIfCollided()[3]);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {}
-  
-// GET INFO FROM PIGEON
 
+  /**
+   * log velocities to calculate acceleration
+   */
+  public void storeVelocities(){
+    rotVelP[0] = rotVelP[1];
+    time[0] = time[1];
+    xVelC[0] = xVelC[1];
+    xVelY[0] = xVelY[1];
+    rotVelC[0] = rotVelC[1];
+
+    rotVelP[1] = Units.degreesToRadians(drivetrain.getPigeon2().getAngularVelocityZDevice().getValueAsDouble());
+    time[1] = Utils.getCurrentTimeSeconds();
+    xVelC[1] = drivetrain.getCurrentRobotChassisSpeeds().vxMetersPerSecond;
+    xVelY[1] = drivetrain.getCurrentRobotChassisSpeeds().vyMetersPerSecond;
+    rotVelC[1] = drivetrain.getCurrentRobotChassisSpeeds().omegaRadiansPerSecond;
+  }
+
+  /**
+   * @return array with pigeon acceleration 
+   * 0 - Acceleration in X direction
+   * 1 - Acceleration in Y direction
+   * 2 - Magnitude of X and Y Acceleration
+   * 3 - Direction of X and Y Acceleration
+   * 4 - Rotational Acceleration
+   */
   public double[] getPigeonAcceleration(){
     double accX = drivetrain.getPigeon2().getAccelerationX().getValueAsDouble() 
     - drivetrain.getPigeon2().getGravityVectorX().getValueAsDouble() // subtract gravity from acceleration
     * CollisionConstants.ACCELERATION_DUE_TO_GRAVITY; // convert g-force to m/s^2
+
     double accY = drivetrain.getPigeon2().getAccelerationY().getValueAsDouble() 
     - drivetrain.getPigeon2().getGravityVectorY().getValueAsDouble() // subtract gravity from acceleration
     * CollisionConstants.ACCELERATION_DUE_TO_GRAVITY; // convert g-force to m/s^2
+
     double accMag = Math.hypot(accX, accY);
     double accDirection = Math.atan2(accY, accX);
-    double accRot = (angularVelocityWorldLog[1] - angularVelocityWorldLog[0]) / (timeLog[1] - timeLog[0]);
+    double accRot = (rotVelP[1] - rotVelP[0]) / (time[1] - time[0]);
     return new double[] {accX, accY, accMag, accDirection, accRot};
   }
 
-// GET CHASSIS SPEEDS (motor probably)
-
+  /**
+   * @return array with chassis acceleration 
+   * 0 - Acceleration in X direction
+   * 1 - Acceleration in Y direction
+   * 2 - Magnitude of X and Y Acceleration
+   * 3 - Direction of X and Y Acceleration
+   * 4 - Rotational Acceleration
+   */
   public double[] getChassisAcceleration(){
-    double accX = (velocityXChassis[1] - velocityXChassis[0]) / (timeLog[1] - timeLog[0]);
-    double accY = (velocityYChassis[1] - velocityYChassis[0] / timeLog[1] - timeLog[0]);
+    double accX = xAccCFilter.calculate((xVelC[1] - xVelC[0]) / (time[1] - time[0]));
+    double accY = yAccCFilter.calculate((xVelY[1] - xVelY[0]) / (time[1] - time[0]));
     double accMag = Math.hypot(accX, accY);
     double accDirection = Math.atan2(accY, accX);
-    double accRot = (velocityRotChassis[1] - velocityRotChassis[0] / timeLog[1] - timeLog[0]);
+    double accRot = rotAccCFilter.calculate((rotVelC[1] - rotVelC[0]) / (time[1] - time[0]));
     return new double[] {accX, accY, accMag, accDirection, accRot};
   }
 
-  // COMPARE CHASIS AND PIGEON
+  public double[] getChassisAcceleration2(){
+    double accX = xAccC2Filter.calculate((xVelC[1] - xVelC[0]) / (time[1] - time[0]));
+    double accY = yAccC2Filter.calculate((xVelY[1] - xVelY[0]) / (time[1] - time[0]));
+    double accMag = Math.hypot(accX, accY);
+    double accDirection = Math.atan2(accY, accX);
+    double accRot = rotAccC2Filter.calculate((rotVelC[1] - rotVelC[0]) / (time[1] - time[0]));
+    return new double[] {accX, accY, accMag, accDirection, accRot};
+  }
 
+  /**
+   * Compare Pigeon and Chassis Acceleraiton
+   * @return array
+   */
   public boolean[] getIfCollided(){
     double differenceX = Math.abs(getPigeonAcceleration()[0] - getChassisAcceleration()[0]);
     boolean xCollided = differenceX > getPigeonAcceleration()[0] * CollisionConstants.ACCELERATION_TOLERANCE 
