9cfde4a026a13503544288571e82c46203d67746
MattD8957 <Dowlmj8957@gmail.com>
2024-03-02 11:45:16 -0500

    [#355] test soon

--- src/main/java/frc/robot/command/ClimbLineUp.java
+++ src/main/java/frc/robot/command/ClimbLineUp.java
@@ -1,5 +1,7 @@
 package frc.robot.command;
 
+import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathPlannerPath;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants.ClimbConstants;
 import frc.robot.subsystems.Swerve;
@@ -7,6 +9,9 @@ import frc.robot.subsystems.Swerve;
 public class ClimbLineUp extends SequentialCommandGroup {
 
 	public ClimbLineUp(Swerve drivetrain) {
-    	addCommands(new MoveToPose(ClimbConstants.BACK_CLIMB, drivetrain));
+		PathPlannerPath path = PathPlannerPath.fromPathFile("FRONT-Climb-Lineup");
+    	addCommands(
+			new MoveToPose(ClimbConstants.BACK_CLIMB, drivetrain), 
+			new AutoBuilder().followPath(path));
   	}
 }
