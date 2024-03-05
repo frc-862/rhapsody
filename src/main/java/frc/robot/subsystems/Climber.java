e72d3f1e7c7ef2fde201bcc2cea62a3bd3c1ae59
Fritz Geib <fritz@standland.org>
2024-03-05 08:34:37 -0500

    [#354] cleanup for merge

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -9,7 +9,6 @@ import com.ctre.phoenix6.signals.FeedbackSensorSourceValue;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.ClimbConstants;
-import frc.robot.Constants.ClimbConstants.CLIMBER_STATES;
 import frc.robot.Constants.RobotMap.CAN;
 import frc.thunder.hardware.ThunderBird;
 import frc.thunder.shuffleboard.LightningShuffleboard;
@@ -19,20 +18,12 @@ public class Climber extends SubsystemBase {
     private ThunderBird climbMotorR;
     private ThunderBird climbMotorL;
 
-    private PositionVoltage setPointControl = new PositionVoltage(0d);
+    private PositionVoltage setPointControlR = new PositionVoltage(0d);
+    private PositionVoltage setPointControlL = new PositionVoltage(0d);
+
     private DutyCycleOut manualControl = new DutyCycleOut(0d);
 
-    private CLIMBER_STATES state = CLIMBER_STATES.STOW;
-    private Swerve drivetrain;
-
-    private boolean hasClimbed = false;
-    private boolean hasStowed = false;
-    private boolean hasGroundedR = false;
-    private boolean hasGroundedL = false;
-
-    public Climber(Swerve drivetrain) {
-
-        this.drivetrain = drivetrain;
+    public Climber() {
         // configure climb motors
         climbMotorR = new ThunderBird(CAN.CLIMB_RIGHT, CAN.CANBUS_FD,
                 ClimbConstants.CLIMB_RIGHT_MOTOR_INVERT, ClimbConstants.CLIMB_MOTOR_STATOR_CURRENT_LIMIT, ClimbConstants.CLIMB_MOTOR_BRAKE_MODE);
@@ -63,7 +54,7 @@ public class Climber extends SubsystemBase {
         climbMotorR.setPosition(0d);
     }
 
-    private void initLogging() { // TODO test and fix once we have climber
+    private void initLogging() {
         LightningShuffleboard.setDoubleSupplier("Climb", "Left Height", () -> getHeightL());
         LightningShuffleboard.setDoubleSupplier("Climb", "Right Height", () -> getHeightR());
         LightningShuffleboard.setDoubleSupplier("Climb", "Left Setpoint", () -> getSetpointL());
@@ -91,22 +82,6 @@ public class Climber extends SubsystemBase {
     }
 
     /**
-     * Set power to left climb motor
-     * @param power power
-     */
-    public void setPowerL(double power) {
-        climbMotorL.set(power);
-    }
-
-    /**
-     * Set power to right climb motor
-     * @param power power
-     */
-    public void setPowerR(double power) {
-        climbMotorL.set(power);
-    }
-
-    /**
      * sets the setpoint of both climb motors
      * @param setPoint setpoint for both climb motors in pulley rotations
      */
@@ -120,8 +95,10 @@ public class Climber extends SubsystemBase {
      * @param rightSetPoint setpoint for right climb motor in pulley rotations
      */
     public void setSetpoint(double leftSetPoint, double rightSetPoint) {
-        climbMotorL.setControl(setPointControl.withPosition(leftSetPoint));
-        climbMotorR.setControl(setPointControl.withPosition(rightSetPoint));
+        setPointControlL = setPointControlL.withPosition(leftSetPoint);
+        setPointControlR = setPointControlR.withPosition(rightSetPoint);
+        climbMotorL.setControl(setPointControlL);
+        climbMotorR.setControl(setPointControlR);
     }
 
     /**
@@ -163,106 +140,24 @@ public class Climber extends SubsystemBase {
      * @return the setpoint of the right climb arm
      */
     public double getSetpointR() {
-        return this.setPointControl.Position;
+        return this.setPointControlR.Position;
     }
 
     /**
      * @return the setpoint of the left climb arm
      */
     public double getSetpointL() {
-        return this.setPointControl.Position;
-    }
-
-    /**
-     * stores state value for climber
-     * @param state
-     */
-    public void setState(CLIMBER_STATES state) {
-        this.state = state;
-    }
-
-    /**
-     * @return current stored state of climber
-     */
-    public CLIMBER_STATES getState() {
-        return state;
-    }
-
-    /**
-     * tell climber if climber has begun climbing
-     * @param hasClimbed boolean if the robot has climbed
-     */
-    public void setHasClimbed(boolean hasClimbed) {
-        this.hasClimbed = hasClimbed;
-    }
-
-    /**
-     * tell climber if the right arm has been fully extended (robot should have returned to ground
-     * after climb)
-     * @param hasGroundedR
-     */
-    public void setHasGroundedR(boolean hasGroundedR) {
-        this.hasGroundedR = hasGroundedR;
-    }
-
-    /**
-     * tell climber if the left arm has been fully extended (robot should have returned to ground
-     * after climb)
-     * @param hasGroundedL
-     */
-    public void setHasGroundedL(boolean hasGroundedL) {
-        this.hasGroundedL = hasGroundedL;
-    }
-
-    /**
-     * tell climber if both arms have been retracted after returning to gound
-     * @param hasStowed
-     */
-    public void setHasStowed(boolean hasStowed) {
-        this.hasStowed = hasStowed;
-    }
-
-    /**
-     * resets has stowed/grounded/tipped values
-     */
-    public void resetHasValues() {
-        hasGroundedL = false;
-        hasGroundedR = false;
-        hasStowed = false;
-        hasClimbed = false;
+        return this.setPointControlL.Position;
     }
 
     @Override
     public void periodic() {
-        // updates height based on limit switches
+        // zeroes height if the limit switch is pressed or position is negative
         for (TalonFX motor : new TalonFX[] {climbMotorR, climbMotorL}) {
             if (motor.getPosition().getValueAsDouble() < 0
                     || motor.getReverseLimit().getValueAsDouble() == 0) {
                 motor.setPosition(0d);
             }
-        }
-
-        // updates states
-        if (hasClimbed && getHeightL() < ClimbConstants.MAX_HEIGHT / 2
-                && getHeightR() < ClimbConstants.MAX_HEIGHT / 2) {
-            state = CLIMBER_STATES.CLIMBED;
-            resetHasValues();
-        }
-
-        // ONCE the robot is completely climbed, the robot will be in the STOW state
-        if (getHeightR() <= ClimbConstants.CLIMB_RETRACTION_TOLERANCE
-                && getHeightL() <= ClimbConstants.CLIMB_RETRACTION_TOLERANCE
-                && !drivetrain.isTipped() && hasStowed) {
-            state = CLIMBER_STATES.STOW;
-            resetHasValues();
-        }
-
-        if (ClimbConstants.MAX_HEIGHT - getHeightR() <= ClimbConstants.CLIMB_EXTENSION_TOLERANCE
-                && ClimbConstants.MAX_HEIGHT
-                        - getHeightL() <= ClimbConstants.CLIMB_EXTENSION_TOLERANCE
-                && !drivetrain.isTipped() && hasGroundedR && hasGroundedL) {
-            state = CLIMBER_STATES.GROUNDED;
-            resetHasValues();
         }
     }
 }
