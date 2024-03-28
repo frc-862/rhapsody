5d2d2882177c20becf865ea1c866fb85009e8638
WindowsVistaisCool <kyle@quackings.com>
2024-03-27 20:01:25 -0400

    Button Box

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -11,6 +11,7 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.GenericHID;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
@@ -84,6 +85,7 @@ public class RobotContainer extends LightningContainer {
 
     public static XboxControllerFilter driver;
     public static XboxControllerFilter coPilot;
+    public static Joystick buttonBox;
 
     // Subsystems
     public Swerve drivetrain;
@@ -124,6 +126,7 @@ public class RobotContainer extends LightningContainer {
         coPilot = new XboxControllerFilter(ControllerConstants.CopilotControllerPort,
                 Constants.ControllerConstants.DEADBAND, -1, 1,
                 XboxControllerFilter.filterMode.SQUARED); // CoPilot controller
+        buttonBox = new Joystick(ControllerConstants.ButtonBoxControllerPort);
 
         drivetrain = TunerConstants.getDrivetrain();
         limelights = new Limelights();
