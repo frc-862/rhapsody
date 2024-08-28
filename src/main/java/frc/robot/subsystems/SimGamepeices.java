63834eaef51ab7002a26195108c847e89d56d933
vilok1 <vgunti777@gmail.com>
2024-08-28 19:44:56 -0400

    [#208] add comments

--- src/main/java/frc/robot/subsystems/SimGamepeices.java
+++ src/main/java/frc/robot/subsystems/SimGamepeices.java
@@ -26,12 +26,19 @@ public class SimGamepeices extends SubsystemBase{
     public Peice heldPeice;
     public Peice dispensedPeice;
 
+    // peice class
     public class Peice {
         public Pose3d pose;
         public boolean isHeld;
         public double timeHeld;
         public double peiceNum; // for publishing
 
+        /**
+         * construct Peice in robot
+         * @param pose
+         * @param isHeld
+         * @param peiceNum
+         */
         public Peice(Pose3d pose, boolean isHeld, double peiceNum) {
             this.pose = pose;
             this.isHeld = isHeld;
@@ -39,6 +46,11 @@ public class SimGamepeices extends SubsystemBase{
             this.peiceNum = peiceNum;
         }
 
+        /**
+         * construct a peice on the feild
+         * @param pose
+         * @param peiceNum
+         */
         public Peice(Pose3d pose, double peiceNum) {
             this.pose = pose;
             this.isHeld = false;
@@ -47,6 +59,15 @@ public class SimGamepeices extends SubsystemBase{
         }
     }
 
+    /**
+     * construct with peices being passed in
+     * @param pivot
+     * @param flywheel
+     * @param collector
+     * @param drivetrain
+     * @param indexer
+     * @param peices
+     */
     public SimGamepeices(PivotRhapsody pivot, Flywheel flywheel, Collector collector, Swerve drivetrain, Indexer indexer, 
         Peice...peices) {
         this.peices = peices;
@@ -56,12 +77,23 @@ public class SimGamepeices extends SubsystemBase{
         this.drivetrain = drivetrain;
         this.indexer = indexer;
 
+        // publish all peices to shuffleboard
         for (Peice peice : peices) {
             publish(peice);
         }
     }
 
+    /**
+     * construct with default peices
+     * @param pivot
+     * @param flywheel
+     * @param collector
+     * @param drivetrain
+     * @param indexer
+     */
     public SimGamepeices(PivotRhapsody pivot, Flywheel flywheel, Collector collector, Swerve drivetrain, Indexer indexer) {
+
+        // peices on feild
         this.peices = new Peice[] {new Peice(PeiceSimConstants.C1B, 0), new Peice(PeiceSimConstants.C2B, 1), 
             new Peice(PeiceSimConstants.C3B, 2), new Peice(PeiceSimConstants.C1R, 3), new Peice(PeiceSimConstants.C2R, 4), 
             new Peice(PeiceSimConstants.C3R, 5), new Peice (PeiceSimConstants.F1B, 6), new Peice(PeiceSimConstants.F2B, 7), 
@@ -73,31 +105,36 @@ public class SimGamepeices extends SubsystemBase{
         this.drivetrain = drivetrain;
         this.indexer = indexer;
 
+        // publish all peices to shuffleboard
         for (Peice peice : peices) {
             publish(peice);
         }
 
+        // add peice in robot
         addPeice(new Peice(new Pose3d(0, 0, 0, new Rotation3d(0, 0, 0)), 11));
         peices[11].isHeld = true;
         heldPeice = peices[11];
     }
 
     public void collect(){
+        // check to see if the robot already has a peice
         if (heldPeice != null) {
           return;
         }
 
         for (Peice peice : peices) {
-            if (Math.hypot(drivetrain.getPose().getX() - peice.pose.getTranslation().getX(), 
-            drivetrain.getPose().getY() - peice.pose.getTranslation().getY())
-            < PeiceSimConstants.COLLECT_DISTANCE && !peice.isHeld && collector.getPower() > 0
-            && drivetrain.getPose().getRotation().getDegrees() < PeiceSimConstants.COLLECT_ANGLE_MAX.getDegrees()
+            if (Math.hypot(drivetrain.getPose().getX() - peice.pose.getTranslation().getX(),
+            drivetrain.getPose().getY() - peice.pose.getTranslation().getY()) // check is peice is in range
+            < PeiceSimConstants.COLLECT_DISTANCE && !peice.isHeld && collector.getPower() > 0 // check is collector is on
+            && drivetrain.getPose().getRotation().getDegrees() < PeiceSimConstants.COLLECT_ANGLE_MAX.getDegrees() // check if peice within the correct angle
             && drivetrain.getPose().getRotation().getDegrees() > PeiceSimConstants.COLLECT_ANGLE_MIN.getDegrees()) {
                 peice.isHeld = true;
                 heldPeice = peice;
                 if (peice == dispensedPeice) {
                     dispensedPeice = null;
                 }
+
+                simBeamBreaks();
                 break;
             }
 
@@ -105,6 +142,9 @@ public class SimGamepeices extends SubsystemBase{
         }
     }
 
+    /**
+     * simulate beambreaks by estimating where the peice is using subsystem speeds
+     */
     public void simBeamBreaks(){
         if (heldPeice == null) {
             return;
@@ -121,31 +161,44 @@ public class SimGamepeices extends SubsystemBase{
             
             if (distanceFromStart >= -0.5 && distanceFromStart < -0.25) {
                 // In Colelector only
+                collector.beamBreak.setIsTriggered(true);
                 distanceFromStart =+ t * collectorSpeed;
             } else if (distanceFromStart >= -0.25 && distanceFromStart < 0.5) {
                 // In Collector and Indexer
+                collector.beamBreak.setIsTriggered(true);
+                indexer.indexerSensorEntry.setIsTriggered(true);
                 distanceFromStart =+ (t * collectorSpeed + t * indexerSpeed) / 2;
             } else if (distanceFromStart >= 0.5 && distanceFromStart < 1.5) {
                 // In Indexer and Flywheel
+                indexer.indexerSensorEntry.setIsTriggered(false);
+                collector.beamBreak.setIsTriggered(false);
                 distanceFromStart =+ (t * indexerSpeed + t * flywheelSpeed) / 2;
             } else {
                 // In Flywheel only
                 distanceFromStart =+ t * flywheelSpeed;
+                indexer.indexerSensorExit.setIsTriggered(true);
             }
         }
+
         shoot();
+        indexer.indexerSensorExit.setIsTriggered(false);
     }
 
+    /**
+     * shoot the peice
+     */
     public void shoot(){
-        
-            System.out.println("Sim shooting");
-            heldPeice.isHeld = false;
-            simShootTraj(heldPeice);
-            heldPeice = null;
-            return;
-        }
+        System.out.println("Sim shooting");
+        heldPeice.isHeld = false;
+        simShootTraj(heldPeice);
+        heldPeice = null;
+        return;
     }
 
+    /**
+     * simulate the trajectory of the peice being shot using kinematics
+     * @param peice
+     */
     public void simShootTraj(Peice peice){
         Pose2d initialPose = drivetrain.getPose();
         double shootAngle = pivot.getAngle();
@@ -173,6 +226,9 @@ public class SimGamepeices extends SubsystemBase{
         }
     }
 
+    /**
+     * dispense a peice from the source if teleop enabled
+     */
     public void dispensePeiceFromSource(){
         if(dispensedPeice != null || DriverStation.isAutonomous() || !DriverStation.isEnabled()){
             return;
@@ -181,6 +237,9 @@ public class SimGamepeices extends SubsystemBase{
         dispensedPeice = peices[peices.length - 1];
     }
 
+    /**
+     * update the pose of the held peice
+     */
     public void updateHeldPeicePose(){
         if (heldPeice == null) {
             return;
@@ -194,6 +253,10 @@ public class SimGamepeices extends SubsystemBase{
             new Rotation3d(0d, pivot.getAngle() * 2 * Math.PI, drivetrain.getPose().getRotation().getRadians()));
     }
 
+    /**
+     * add peice to the array of peices
+     * @param peice
+     */
     public void addPeice(Peice peice){
         Peice[] newPeices = new Peice[peices.length + 1]; 
         for (int i = 0; i < peices.length; i++) {
@@ -204,6 +267,10 @@ public class SimGamepeices extends SubsystemBase{
         publish(peice);
     }
 
+    /**
+     * delete peice from the array of peices
+     * @param peice
+     */
     public void deletePeice(Peice peice){
         Peice[] newPeices = new Peice[peices.length - 1];
         int j = 0;
@@ -216,6 +283,10 @@ public class SimGamepeices extends SubsystemBase{
         peices = newPeices;
     }
 
+    /**
+     * publish the peice to shuffleboard
+     * @param peice
+     */
     public void publish(Peice peice){
         LightningShuffleboard.setDoubleArray("Peices", "peice #" + peice.peiceNum, 
             () -> new double[] {peice.pose.getX(), peice.pose.getY(), peice.pose.getZ(),
@@ -225,7 +296,6 @@ public class SimGamepeices extends SubsystemBase{
 
     
     public void periodic(){
-        shoot();
         collect();
         dispensePeiceFromSource();
         updateHeldPeicePose();
